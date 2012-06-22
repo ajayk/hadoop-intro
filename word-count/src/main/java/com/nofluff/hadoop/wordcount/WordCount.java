d909fcd2066c37b3796d682666b4e56454f59281
Tim Berglund <tlberglund@gmail.com>
2012-06-22 08:21:47 -0600

    Fixed for 0.20.x

--- word-count/src/main/java/com/nofluff/hadoop/wordcount/WordCount.java
+++ word-count/src/main/java/com/nofluff/hadoop/wordcount/WordCount.java
@@ -17,23 +17,22 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 public class WordCount {
   
   public static void main(String[] args) throws Exception {
-     Configuration conf = new Configuration();
-
-     Job job = new Job(conf, "wordcount");
-
-     job.setOutputKeyClass(Text.class);
-     job.setOutputValueClass(IntWritable.class);
-
-     job.setMapperClass(Map.class);
-     job.setReducerClass(Reduce.class);
-
-     job.setInputFormatClass(TextInputFormat.class);
-     job.setOutputFormatClass(TextOutputFormat.class);
-
-     FileInputFormat.addInputPath(job, new Path(args[0]));
-     FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-     job.waitForCompletion(true);
+    Configuration conf = new Configuration();
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+    if (otherArgs.length != 2) {
+      System.err.println("Usage: wordcount <in> <out>");
+      System.exit(2);
+    }
+    Job job = new Job(conf, "word count");
+    job.setJarByClass(WordCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+    FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
   
 }
