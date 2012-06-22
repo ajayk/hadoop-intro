7e2fb09818bbdb8b5f9c3f747e2d1d8f4faf6743
Tim Berglund <tlberglund@gmail.com>
2012-06-22 07:18:58 -0600

    Added Word Count code

---
+++ word-count/src/main/java/com/nofluff/hadoop/wordcount/Map.java
@@ -0,0 +1,31 @@
+package com.nofluff.hadoop.wordcount;
+
+
+import java.io.IOException;
+import java.util.*;
+        
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+
+public class Map 
+  extends Mapper<LongWritable, Text, Text, IntWritable> {
+   private final static IntWritable one = new IntWritable(1);
+   private Text word = new Text();
+
+   public void map(LongWritable key, Text value, Context context) 
+    throws IOException, InterruptedException {
+       String line = value.toString();
+       StringTokenizer tokenizer = new StringTokenizer(line);
+       while (tokenizer.hasMoreTokens()) {
+           word.set(tokenizer.nextToken());
+           context.write(word, one);
+       }
+   }
+} 
